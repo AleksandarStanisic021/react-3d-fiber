e984a9672b921535b9de0ac72c426d40b3f3e56a
Aleksandar Stanisic <ciandre23@gmail.com>
2026-09-17 18:01:07 +0200

    fix

--- src/Experiance.jsx
+++ src/Experiance.jsx
@@ -3,9 +3,17 @@ import { color } from "three/tsl";
 const Experiance = () => {
   return (
     <>
-      <mesh scale={[1.3, 1, 1]}>
+      <mesh position-x={-2}>
         <sphereGeometry args={[1.3, 32, 32]} />
         <meshBasicMaterial args={[{ color: "crimson" }]} />
+      </mesh>
+      <mesh position-x={2}>
+        <boxGeometry />
+        <meshBasicMaterial args={[{ color: "blue" }]} />
+      </mesh>
+      <mesh scale={[3, 2, 2]} rotateX={45}>
+        <planeGeometry />
+        <meshBasicMaterial args={[{ color: "green" }]} />
       </mesh>
     </>
   );
