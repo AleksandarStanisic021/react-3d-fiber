8164fdde0db62baff6c8aa8830d47f94107a10ca
Aleksandar Stanisic <ciandre23@gmail.com>
2026-09-17 15:47:10 +0200

    fixing errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
 import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+
+function Box() {
+  return (
+    <mesh rotation={[0.5, 0.8, 0]}>
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color="hotpink" />
+    </mesh>
+  );
+}
 
 function App() {
-  console.log(Canvas);
   return (
-    <>
-      <div>Yo</div>
-    </>
+    <div style={{ width: "100vw", height: "100vh" }}>
+      <Canvas camera={{ position: [0, 0, 5] }}>
+        <ambientLight intensity={1.2} />
+        <directionalLight position={[3, 3, 3]} intensity={1.5} />
+        <Box />
+        <OrbitControls enableZoom />
+      </Canvas>
+    </div>
   );
 }
 
