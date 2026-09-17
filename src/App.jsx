5a44944bcaf215d04b7b8b8bc397990150539cfa
Aleksandar Stanisic <ciandre23@gmail.com>
2026-09-17 15:54:06 +0200

    add mesh

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
+import { TorusKnotGeometry } from "three";
 
 function App() {
   return (
     <div style={{ width: "100vw", height: "100vh" }}>
-      <Canvas></Canvas>
+      <Canvas>
+        <mesh>
+          <torusKnotGeometry />
+          <meshNormalMaterial />
+        </mesh>
+      </Canvas>
     </div>
   );
 }
