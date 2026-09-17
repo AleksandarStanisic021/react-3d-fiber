a0219648c1820c0b7f940b410aa72602870309bf
Aleksandar Stanisic <ciandre23@gmail.com>
2026-09-17 18:44:08 +0200

    animate cube

--- src/Experiance.jsx
+++ src/Experiance.jsx
@@ -1,13 +1,20 @@
+import { useFrame } from "@react-three/fiber";
+import { useRef } from "react";
 import { color } from "three/tsl";
 
 const Experiance = () => {
+  const boxRef = useRef();
+
+  useFrame(() => {
+    boxRef.current.rotation.y += +0.03;
+  });
   return (
     <>
       <mesh position-x={-2}>
         <sphereGeometry />
         <meshBasicMaterial args={[{ color: "crimson" }]} />
       </mesh>
-      <mesh scale={1} position={[2, 0, 2]}>
+      <mesh ref={boxRef} scale={1} position={[2, 0, 2]}>
         <boxGeometry />
         <meshBasicMaterial args={[{ color: "blue" }]} />
       </mesh>
