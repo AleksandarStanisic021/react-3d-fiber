46912be57cc8723b3605a5189148d543cb734b48
Aleksandar Stanisic <ciandre23@gmail.com>
2026-09-17 17:00:16 +0200

    refactor code

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
+import { Canvas } from "@react-three/fiber";
 import Experiance from "./Experiance";
 
 function App() {
   return (
-    <>
-      <div
-        style={{
-          width: "100%",
-          height: "100%",
-          position: "fixed",
-          top: "0px",
-        }}>
+    <div
+      style={{
+        width: "100%",
+        height: "100%",
+        position: "fixed",
+        top: "0px",
+      }}>
+      <Canvas>
         <Experiance />
-      </div>
-    </>
+      </Canvas>
+    </div>
   );
 }
 
--- src/Experiance.jsx
+++ src/Experiance.jsx
@@ -1,16 +1,10 @@
-import { Canvas } from "@react-three/fiber";
-import { OrbitControls } from "@react-three/drei";
-import { TorusKnotGeometry } from "three";
-
 const Experiance = () => {
   return (
     <>
-      <Canvas>
-        <mesh>
-          <torusKnotGeometry />
-          <meshNormalMaterial />
-        </mesh>
-      </Canvas>
+      <mesh>
+        <torusKnotGeometry />
+        <meshNormalMaterial />
+      </mesh>
     </>
   );
 };
