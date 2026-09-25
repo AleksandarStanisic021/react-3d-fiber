abe230552e7b116b8930b85106ed41f7e3255bd4
Aleksandar Stanisic <ciandre23@gmail.com>
2026-09-25 20:29:59 +0200

    feat: update Sky color to red and reposition box meshes for improved scene layout

--- src/Scene.jsx
+++ src/Scene.jsx
@@ -5,7 +5,7 @@ const Scene = () => {
     <>
       <directionalLight castShadow shadow-mapSize={1024} position={[1, 2, 3]} />
       <ambientLight color={"white"} intensity={0.5} />
-      <Sky />
+      <Sky color="red" />
       <OrbitControls
         makeDefault
         enableDamping
@@ -22,9 +22,14 @@ const Scene = () => {
         <meshStandardMaterial args={[{ color: "orange" }]} />
       </mesh>
 
-      <mesh castShadow receiveShadow scale={1} position={[1.5, -0.5, 1]}>
+      <mesh castShadow receiveShadow scale={1} position={[-2.5, -0.5, 1]}>
         <boxGeometry />
         <meshStandardMaterial args={[{ color: "red" }]} />
+      </mesh>
+
+      <mesh castShadow receiveShadow scale={1} position={[2.5, -0.5, 1]}>
+        <boxGeometry />
+        <meshStandardMaterial args={[{ color: "blue" }]} />
       </mesh>
 
       <mesh
