1b84fef9d9ff792e9f676a757434123da60c3fc5
Aleksandar Stanisic <ciandre23@gmail.com>
2026-09-25 18:33:13 +0200

    feat: add onCreated handler to Canvas for custom clear color in App component

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,14 @@ import Experiance from "./Experiance";
 import Exp from "./Exp";
 import Scene from "./Scene";
 import Debug from "./Debug";
+import { cloneUniforms } from "three/src/renderers/shaders/UniformsUtils.js";
 
 function App() {
+  const created = ({ gl }) => {
+    console.log(gl);
+    gl.setClearColor("#1e1efe");
+  };
+
   return (
     <div
       style={{
@@ -13,7 +19,7 @@ function App() {
         position: "fixed",
         top: "0px",
       }}>
-      <Canvas>
+      <Canvas onCreated={created}>
         <Scene />
       </Canvas>
     </div>
