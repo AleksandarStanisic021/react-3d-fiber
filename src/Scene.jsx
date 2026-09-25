c6bf969f9091ea456ad07f1519b2bd4f24a15056
Aleksandar Stanisic <ciandre23@gmail.com>
2026-09-25 20:24:57 +0200

    feat: enhance Scene component with shadow settings and add Sky for improved environment

--- src/Scene.jsx
+++ src/Scene.jsx
@@ -1,10 +1,11 @@
-import { OrbitControls, MeshReflectorMaterial } from "@react-three/drei";
+import { Sky, OrbitControls, MeshReflectorMaterial } from "@react-three/drei";
 
 const Scene = () => {
   return (
     <>
-      <directionalLight castShadow position={[1, 2, 3]} />
+      <directionalLight castShadow shadow-mapSize={1024} position={[1, 2, 3]} />
       <ambientLight color={"white"} intensity={0.5} />
+      <Sky />
       <OrbitControls
         makeDefault
         enableDamping
@@ -16,12 +17,12 @@ const Scene = () => {
         enablePan={false}
       />
 
-      <mesh castShadow position-x={-2}>
+      <mesh castShadow receiveShadow position->
         <sphereGeometry />
         <meshStandardMaterial args={[{ color: "orange" }]} />
       </mesh>
 
-      <mesh castShadow scale={1} position={[2, -0.5, 2]}>
+      <mesh castShadow receiveShadow scale={1} position={[1.5, -0.5, 1]}>
         <boxGeometry />
         <meshStandardMaterial args={[{ color: "red" }]} />
       </mesh>
